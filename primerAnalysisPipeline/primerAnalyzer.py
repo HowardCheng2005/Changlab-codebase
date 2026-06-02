4d570dd967284c7158f09b859949604b5582a8c2
HowardCheng <howardhx.cheng@mail.utoronto.ca>
2026-06-02 16:02:42 -0400

    factors in top n scores now

--- primerAnalysisPipeline/primerAnalyzer.py
+++ primerAnalysisPipeline/primerAnalyzer.py
@@ -34,30 +34,41 @@ def token_updater(access_token, expire_time):
     return access_token, expire_time
 
 def self_dimer_scoring (primer, combination, token):
-    # Find self-dimerization scores for the chosen combination
+    # Find top self-dimerization scores for the chosen combination
     sequence = primer + combination
-    self_dimer = self_dimerization(sequence, token)[0]
-    score = self_dimer["DeltaG"]
+    self_dimer = self_dimerization(sequence, token)
+    top_ends = []
+    top_scores = []
 
-    # for 3' end binding analysis
-    binding = self_dimer["Bonds"]
-    top_padding = self_dimer["TopLinePadding"]
-    bond_padding = self_dimer["BondLinePadding"]
-    bottom_padding = self_dimer["BottomLinePadding"]
-    top_end, bottom_end = binding_location_calculator(
-        sequence,
-        binding,
-        top_padding,
-        bond_padding,
-        bottom_padding,
-        OFFSET
-    )
-    print(f"self dimerization score for {combination}: {score}")
+    for i in range(top_reads):
 
-    # format for binding sequence analysis
-    self_dimer_output = [(top_end + bottom_end), score, sequence]
+        # self-dimerization delta G score for the ith top interaction
+        top_self_dimer = self_dimer[i]
+        score = top_self_dimer["DeltaG"]
 
-    return self_dimer_output
+        # for 3' end binding analysis
+        binding = top_self_dimer["Bonds"]
+        top_padding = top_self_dimer["TopLinePadding"]
+        bond_padding = top_self_dimer["BondLinePadding"]
+        bottom_padding = top_self_dimer["BottomLinePadding"]
+        top_end, bottom_end = binding_location_calculator(
+            sequence,
+            binding,
+            top_padding,
+            bond_padding,
+            bottom_padding,
+            OFFSET
+        )
+        print(f"self dimerization score for {combination}: {score}")
+
+        # adds ith dimerization 3' end score and dimerization delta G score
+        top_ends.append(top_end + bottom_end)
+        top_scores.append(score)
+
+    # adds the 3' end and delta-G means of the top n reads to the output
+    output_scoring = [sum(top_ends)/top_reads, sum(top_scores)/top_reads, sequence]
+
+    return output_scoring
 
 def hairpin_scoring (primer, combination, token):
 
@@ -79,6 +90,7 @@ if __name__ == "__main__":
     parser.add_argument("-bd", "--bind", required=False, default=0, type=int, help="binding dimerization location weight")
     parser.add_argument("hp", "--hairpin", required=False, default=0, type=int, help="hairpin formation weight")
     parser.add_argument("-l", "--length", required=True, type=int, help="length of binding sequence")
+    parser.add_argument("-t", "--topn", type=int, required=False, default=1, help="range of top n binding sequences in dimerization")
     parser.add_argument("-i", "--id", required=True, help="id of the IDT account")
     parser.add_argument("-sc", "--secret", required=True, help="secret of the IDT account")
     parser.add_argument("-u", "--username", required=True, help="username of the IDT account")
@@ -95,6 +107,9 @@ if __name__ == "__main__":
     reverse_primer = args.reverse_primer
     binding_sequence_length = args.length
     output_file = args.output
+
+    #average top n reads used in the dimerization proces
+    top_reads = args.topn
 
     # accesses IDT API
     idt_identity = args.id
@@ -174,33 +189,48 @@ if __name__ == "__main__":
             forward_self_end, forward_self_score, forward_self_sequence, forward_hairpin = forward_dict[forward_combination]
             reverse_self_end, reverse_self_score, reverse_self_sequence, reverse_hairpin = reverse_dict[reverse_combination]
 
-            # Uses API for heterogeneous binding scores
-            het_dimerization = hetero_dimerization(forward_self_sequence, reverse_self_sequence, access_token)[0]
-            het_score = het_dimerization["DeltaG"]
+            # Uses API for top n heterogeneous binding scores
+            het_dimerization = hetero_dimerization(forward_self_sequence, reverse_self_sequence, access_token)
+            top_het_ends = []
+            top_het_scores = []
 
-            # for binding analysis
-            het_binding = het_dimerization["Bonds"]
-            het_top_padding = het_dimerization["TopLinePadding"]
-            het_bond_padding = het_dimerization["BondLinePadding"]
-            het_bottom_padding = het_dimerization["BottomLinePadding"]
-            het_end_forward, het_end_reverse = binding_location_calculator(
-                forward_self_sequence,
-                het_binding,
-                het_top_padding,
-                het_bond_padding,
-                het_bottom_padding,
-                OFFSET
-            )
+            for j in range(top_reads):
+                # top ith heterogeneous dimerization interaction
+                top_het_dimerization = het_dimerization[j]
 
-            print(f"hetero dimerization score for {forward_combination} and {reverse_combination}: {het_score}")
+                # top ith dimerization delta G scoring
+                het_score = top_het_dimerization["DeltaG"]
+
+                # for binding analysis
+                het_binding = top_het_dimerization["Bonds"]
+                het_top_padding = top_het_dimerization["TopLinePadding"]
+                het_bond_padding = top_het_dimerization["BondLinePadding"]
+                het_bottom_padding = top_het_dimerization["BottomLinePadding"]
+                het_end_forward, het_end_reverse = binding_location_calculator(
+                    forward_self_sequence,
+                    het_binding,
+                    het_top_padding,
+                    het_bond_padding,
+                    het_bottom_padding,
+                    OFFSET
+                )
+
+                print(f"hetero dimerization score for {forward_combination} and {reverse_combination}: {het_score}")
+
+                top_het_ends.append(het_end_forward + het_end_reverse)
+                top_het_scores.append(het_score)
+
+            #average of the top n scores and ends
+            top_het_ends = sum(top_het_ends)/top_reads
+            top_het_scores = sum(top_het_scores)/top_reads
 
             # find final combined scores for each forward and reverse combination.
             combined_score = (self_weight * forward_self_score) + (self_weight * reverse_self_score) + (
-                het_weight * het_score)
+                het_weight * top_het_scores)
 
             # IF bd != 0, factors in whether the dimerization occurs in the last 15 nucleotides or not for both self-dimerization and heterogeneous dimerization for the model
             combined_score -= binding_weight * (self_weight * (forward_self_end + reverse_self_end) +
-                                                het_weight * (het_end_reverse + het_end_forward))
+                                                het_weight * top_het_ends)
 
             # IF hp != 0, factors in whether hairpin formation in the forward and reverse reads for the model
             combined_score -= hairpin_weight * (forward_hairpin + reverse_hairpin)
@@ -213,8 +243,8 @@ if __name__ == "__main__":
                 "forward_3_end": forward_self_end,
                 "reverse_self_dimer": reverse_self_score,
                 "reverse_3_end": reverse_self_end,
-                "het_dimer": het_score,
-                "het_3_end": f"{het_end_forward}, {het_end_reverse}",
+                "het_dimer": top_het_scores,
+                "het_3_end": top_het_ends,
                 "combined_score": combined_score,
                 "forward_hairpin": forward_hairpin,
                 "reverse_hairpin": reverse_hairpin,
