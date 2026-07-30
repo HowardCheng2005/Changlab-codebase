f407a7954d7067b456a6516128b732e65ea52cfb
HowardCheng <howardhx.cheng@mail.utoronto.ca>
2026-07-30 13:28:08 -0400

    added total reads

--- illuminaReadAnalysis/pacbioLibraryProportion.py
+++ illuminaReadAnalysis/pacbioLibraryProportion.py
@@ -17,24 +17,28 @@ def pacbio_cluster_dictionary_creator(pacbio_dict):
 
     return cluster_dictionary
 
-def check_library_read_proportion(barcode_file, pacbio_dict,cluster_dict):
+def check_library_read_proportion(barcode_file, pacbio_dict, cluster_dict):
     df = pd.read_csv(barcode_file)
 
     num_unique_library_reads = 0
-    num_without_pacbio = 0
+    num_unique_without_pacbio = 0
+    num_total_library_reads = 0
+    num_total_without_pacbio = 0
 
     reads = df['Unique.reads']
+    frequency = df['Frequency']
     clusters = df['Cluster.ID']
 
     for i, read in enumerate(reads):
         if read not in pacbio_dict:
             num_unique_library_reads += 1
+            num_total_library_reads += frequency[i]
 
             if clusters[i] not in cluster_dict:
-                num_without_pacbio += 1
+                num_unique_without_pacbio += 1
+                num_total_without_pacbio += frequency[i]
 
-    return num_unique_library_reads, num_without_pacbio
-
+    return num_unique_library_reads, num_unique_without_pacbio, num_total_library_reads, num_total_without_pacbio
 
 
 if __name__ == "__main__":
@@ -42,6 +46,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input", dest="input", required=True, help="input pacbio to cluster .json file")
     parser.add_argument("-b", "--barcode", dest="barcode", required=True, help="barcode file in .csv format containing pacbio + library reads")
     parser.add_argument("-o", "--output", dest="output", required=True, help="output file for clusters with no pacbio")
+
 
     args = parser.parse_args()
 
@@ -52,9 +57,11 @@ if __name__ == "__main__":
     pacbio_dictionary = pacbio_dictionary_creator(input)
     cluster_dictionary = pacbio_cluster_dictionary_creator(pacbio_dictionary)
 
-    unique_reads, without_pacbio_reads = check_library_read_proportion(barcode, pacbio_dictionary, cluster_dictionary)
+    unique_reads, unique_without_pacbio_reads, total_reads, total_without_pacbio_reads = check_library_read_proportion(barcode, pacbio_dictionary, cluster_dictionary)
 
     with open(output, "w") as f:
         f.write(f"Unique reads: {unique_reads}\n")
-        f.write(f"No pacbio: {without_pacbio_reads}\n")
+        f.write(f"Unique no pacbio: {unique_without_pacbio_reads}\n")
+        f.write(f"Total reads: {total_reads}\n")
+        f.write(f"Total without pacbio: {total_without_pacbio_reads}\n")
         f.close()
